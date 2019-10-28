4af38a4154195ea5380c9a8faef7e58db3849b27
Cibin S B <cibin345@gmail.com>
2019-10-28 11:42:32 +0530

    Updating Read me and test cases

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# EMBL task
+EMBL-EBI test
+==============================
+
+
+
+What Is This?
+-------------
+
+This is a simple Python/Falcon application intended to provide a working example of searching EMBL database <b>ensembl_website_97</b>. The goal of these endpoints is to be simple, well-documented and to provide a base for developers to develop other applications off of.
+
+
+How To start the API service using docker
+---------------
+Run the following commands 
+1. docker-compose build
+2. docker-compose up
+
+The service will start to listen on port <b>8000</b>
+
+Testing
+-------
+
+1. Login to the `app_server` docker container 
+2. Run the command `python -m unittest test/api_test.py`
+
+
+Making Requests
+---------------
+
+Please go to `http://localhost:8000/swagger` to find more about the API documentation. 
--- test/api_test.py
+++ test/api_test.py
@@ -2,6 +2,7 @@ import unittest
 import falcon
 from falcon import testing
 from app import api
+from helper.log import logger
 
 
 class EMBLTestCase(testing.TestCase):
@@ -9,17 +10,20 @@ class EMBLTestCase(testing.TestCase):
         super(EMBLTestCase, self).setUp()
         self.app = api
 
-class TestMyApp(EMBLTestCase):
+class TestEMBL(EMBLTestCase):
     def test_get_all_records(self):
+        logger.info("Searching for gene name tbx")
         result = self.simulate_get('/api/v1/embl/search?name=tbx')
         self.assertEqual(result.status, falcon.HTTP_200)
 
     def test_validate_params(self):
+        logger.info("Validating parameters")
         output={u"error": u"Search param 'name' cannot be none or less than 3 chars"}
         result = self.simulate_get('/api/v1/embl/search?name=tb')
         self.assertEqual(result.json, output)
 
     def test_http_methods(self):
+        logger.info("Testing Http methods like POST, PUT, DELETE")
         result = self.simulate_post('/api/v1/embl/search?name=tbx')
         self.assertEqual(result.status, falcon.HTTP_405)
         result = self.simulate_put('/api/v1/embl/search?name=tbx')
